f09451ddb67fd970b43765d0b63e1f12b3996f95
rajeshlakkanaboina <rajeshlakkanaboina@gmail.com>
2026-07-08 14:47:35 +0530

    Add files via upload

---
+++ faq_data.py
@@ -0,0 +1,19 @@
+faqs = {
+    "What is Python?":
+        "Python is a high-level programming language.",
+
+    "Who developed Python?":
+        "Python was developed by Guido van Rossum.",
+
+    "What is machine learning?":
+        "Machine learning is a branch of artificial intelligence.",
+
+    "What is NLP?":
+        "NLP stands for Natural Language Processing.",
+
+    "What is CodeAlpha?":
+        "CodeAlpha provides internship opportunities for students.",
+
+    "How can I apply for internship?":
+        "You can apply for internships through the CodeAlpha website."
+}
